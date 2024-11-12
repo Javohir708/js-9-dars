507d26b2d6482f7be971a64f35aface9a6907744
Javohir708 <joxauzking@gamil.com>
2024-11-12 18:22:47 +0500

    Homework

---
+++ app.js
@@ -0,0 +1,68 @@
+// Soatni ko'rsatish uchun.
+const timeEl = document.querySelector('.time')
+// console.log(timeEl);
+
+
+function getTime () {
+    let date = new Date()
+    
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    let timeView = `${hour}:${minute}:${second}`
+    // console.log(timeView);
+
+    timeEl.innerHTML = timeView
+}
+
+// getTime()
+
+setInterval ( () => {
+    getTime()
+}, 1000 )
+
+
+// Kunni ko'rsatish uchun
+const day = document.querySelector('.day')
+
+function getDay () {
+    let days = new Date()
+    let kun = days.getDay()
+
+    let dayName = ['Yakshanba', 'Dushanba', 'Seshanba', 'Chorshanba', 'Payshanba', 'Juma', 'Shanba']
+ 
+    let dayView = dayName[kun]
+
+    day.innerHTML = dayView
+}
+
+// getDay()
+
+setInterval ( () => {
+    getDay()
+}, 1000)
+
+
+// Sana va yilni ko'rsatish uchun
+const date = document.querySelector('.date')
+
+function getDateYear () {
+    let dates = new Date()
+
+    let sana = dates.getDate()
+    let oy = dates.getMonth()
+    let yil = dates.getFullYear()
+
+    let months = ['Yanvar', 'Fevral', 'Mart', 'Aprel', 'May', 'Iyun', 'Iyul', 'Avgust', 'Sentabr', 'Oktabr', 'Noyabr', 'Dekabr']
+
+    let dateView = `${sana}-${months[oy]}. ${yil}`
+
+    date.innerHTML = dateView
+}
+
+// getDateYear()
+
+setInterval ( () => {
+    getDateYear()
+}, 1000)
